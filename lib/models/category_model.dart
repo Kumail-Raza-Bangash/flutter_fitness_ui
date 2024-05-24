6d8c086aa9633c3f5d6d8ac87c158fdcff14f496
Kumail-Raza-Bangash <kumailraza260@gmail.com>
2024-05-24 10:53:43 +0500

    Updated Code

---
+++ lib/models/category_model.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fitness_ui/resources/app_color.dart';
+
+class CategoryModel {
+  String name;
+  String iconPath;
+  Color boxColor;
+
+  CategoryModel({
+    required this.name,
+    required this.iconPath,
+    required this.boxColor,
+  });
+
+  List<CategoryModel> getCategories() {
+    List<CategoryModel> categories = [];
+
+    categories.add(
+      CategoryModel(
+        name: "Salad",
+        iconPath: "assets/images/salad.png",
+        boxColor: AppColor.secondaryColor,
+      ),
+    );
+
+    categories.add(
+      CategoryModel(
+        name: "Cake",
+        iconPath: "assets/images/cake.png",
+        boxColor: AppColor.secondaryColor,
+      ),
+    );
+
+    categories.add(
+      CategoryModel(
+        name: "Pie",
+        iconPath: "assets/images/pie.png",
+        boxColor: AppColor.secondaryColor,
+      ),
+    );
+
+    categories.add(
+      CategoryModel(
+        name: "Smoothies",
+        iconPath: "assets/images/smoothies.png",
+        boxColor: AppColor.secondaryColor,
+      ),
+    );
+
+    return categories;
+  }
+}
--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_fitness_ui/resources/app_color.dart';
 import 'package:flutter_fitness_ui/view/widgets/custom_icon.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -17,8 +18,36 @@ class _HomePageState extends State<HomePage> {
       appBar: _appBar(),
       backgroundColor: AppColor.whiteColor,
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           _searchField(),
+          const SizedBox(height: 40),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 20.0),
+                child: Text(
+                  "Category",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Container(
+                height: 150,
+                color: Colors.green,
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Container();
+                  },
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
